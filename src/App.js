8f723d5bf209a5b5ec5fb329784442975302c1bf
Povalinna <111307356+Povalinna@users.noreply.github.com>
2023-07-17 12:56:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
-import React from "react";
-import Example from "./Example"
-
-
-//const App=()=> {
-// return (
-//   <div className="App">
-//    <h4>App</h4>
-//  </div>
-//);
-//}
-
-//export default App;
+import React from 'react';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      country: "Ukraine"
+        this.state = {
+            postId: null,
+            titleId: ``,
+
+        };
     }
-  }
 
-  changCountry = () => {
-    this.setState({ country: this.state.country === "Ukraine is the best country" ? "Ukraine" : "Ukraine is the best country" })
-  }
+    componentDidMount() {
 
-  render() {
-    return (
-      <>
-        <div className="App">
-          <button onClick={this.changCountry}>click</button>
-        </div>
+        let postNumber = Number(prompt(`input the number of post`));
 
-        <Example
-          subject={this.state.country}
-        />
-      </>
-    )
-  }
+        let link = `https://jsonplaceholder.typicode.com/todos/` + JSON.parse(postNumber)
+        console.log(link)
+        fetch(link)
+            .then(response => response.json())
+            .then(data => {
+                console.log({ data })
+                this.setState({ data })
 
+                this.setState({ titleId: data.title, postId: data.id });
+            })
 }
 
-export default App;
+ render() {
+        const { titleId, postId } = this.state;
+
+return (
+            <div className="card text-center m-3">
+                <h5 className="card-header">posts</h5>
+                <ul>
+
+        <li key={postId} >
+                        {titleId}
+                       
+                        <button>delete</button>
+                        <button>edit</button>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default App;; 
+ 
