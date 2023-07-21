ae968e357a289d62f9b520dea3fa07b35805935d
Povalinna <111307356+Povalinna@users.noreply.github.com>
2023-07-21 19:08:08 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,123 @@
-import React from "react";
-import Example from "./Example"
+import React, { Component } from 'react';
+
+class App extends Component {
+
+   constructor(props) {
+      super(props);
+      this.state = {
+         id:``,
+         userid:``,
+         title:``,
+         posts:[],
+         body:``,
+         data:[],
+         
+      };
+   }
+  
+   handleSubmit = (e) => {
+      e.preventDefault();
+   }
+ handleChange=(event)=>{
+     
+     this.setState({value:event.target.value});
+      const id = event.target.value;
+      let link = "https://jsonplaceholder.typicode.com/posts?_limit="+ JSON.parse(id);
+    // let link="https://jsonplaceholder.typicode.com/" +JSON.parse(posts) ;
+     console.log(link)
+       fetch(link)
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            this.setState({data:data});
+           this.setState({ posts: [ data,...this.state.posts] });
+         
+            this.setState({ title: data.title });
+            this.setState({ body: data.body});
+           this.setState({id:data.id})
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+         
+   };
+   
+      handleButtonClear = (e) => {
+         e.preventDefault();
+         this.setState({value:``})
+      }
 
 
-//const App=()=> {
-// return (
-//   <div className="App">
-//    <h4>App</h4>
-//  </div>
-//);
-//}
-
-//export default App;
-
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      country: "Ukraine"
-    }
-  }
-
-  changCountry = () => {
-    this.setState({ country: this.state.country === "Ukraine is the best country" ? "Ukraine" : "Ukraine is the best country" })
-  }
-
-  render() {
-    return (
-      <>
-        <div className="App">
-          <button onClick={this.changCountry}>click</button>
-        </div>
-
-        <Example
-          subject={this.state.country}
-        />
-      </>
-    )
-  }
-
+      handleDelete = (e)=>{
+         e.preventDefault();
+         this.setState({values:e.target.values});
+         const id = e.target.value;
+         console.log(id);
+         this.setState(this.state.data.splice((id-1), 1));
+         console.log(this.state.data)
+      }
+        
+      
+       handleEdit=(e)=>{
+         e.preventDefault();
+         this.setState({valuess:e.target.valuess});
+         const body = JSON.stringify(e.target.valuess);
+         console.log(body);
+         return body;
+       }
+       handleEditClick(id,body){
+         this.setState(this.state.data.splice(id, 1,(body))); 
+         
 }
+  
+  // console.log(id);
+   //let link = "https://jsonplaceholder.typicode.com/posts?id=" + JSON.parse(id);
+   //console.log(link);
+  // fetch(link,
+  // {method: 'DELETE',
+  //  });
+  
 
+
+   render() {
+      const { data  } = this.state;
+      const { title, posts, body,id } =  this.state;
+
+    
+return (
+   <div className="app">
+   <form onSubmit={this.handleSubmit}>
+         <p>input the amount of posts</p> 
+  
+            <input type="text" value={this.state.value} onChange={this.handleChange}/>
+          <button type="submit" value="Submit" >submit</button>
+          <button values={this.state.values} onClick={this.handleButtonClear}>reset</button>
+          </form>
+         <div>
+         {posts.length > 0 && (
+         <ul>
+             {data.map(data =>(
+               <li key={data.id}>{data.body};{data.id}
+               <br></br>
+               <input valuess={this.state.valuess} onChangeCapture={this.handleEdit}/>
+         <button  id={data.id} onClickCapture={()=>this.handleEditClick(id)}>edit</button>
+                
+              
+               </li>
+               
+
+               ))}
+             </ul>
+         )}
+         <div>
+            <p>If want to delete the post - input id of the post</p>
+            <input valuess={this.state.valuess} onChangeCapture={this.handleDelete}/>
+         <button  >delete</button>
+         
+         </div>
+      </div>
+      </div>
+)}
+             }        
+                    
 export default App;
